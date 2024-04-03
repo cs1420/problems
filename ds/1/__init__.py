bc4bf70c074246ebaeab559a63caa98bbe70ca9b
GRudolph <george.rudolph@uvu.edu>
2024-04-02 22:27:05 -0600

    Update __init__.py

--- ds/1/__init__.py
+++ ds/1/__init__.py
@@ -17,7 +17,7 @@ def student_test():
 def setup(self):
    """Setup"""
    # Replace the student's test file with your own
-   add("staff_test_desserts.py")
+   check50.add("staff_test_desserts.py")
 
 @check50.check(setup)
 def test_candy_default():
