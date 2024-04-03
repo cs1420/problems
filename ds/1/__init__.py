5e602e2671976c35942cc27574414a14d3ad513e
GRudolph <george.rudolph@uvu.edu>
2024-04-02 19:39:41 -0600

    Update __init__.py

--- ds/1/__init__.py
+++ ds/1/__init__.py
@@ -5,7 +5,7 @@ def exists():
     """Dessert Shop 1 files exist."""
     check50.exists("desserts.py")
     check50.exists("test_desserts.py")
-
+```
 @check50.check(exists)
 def test_candy_default():
   from desserts import Candy
@@ -183,4 +183,4 @@ def check_expect(targets):
   for expected, actual, help in targets:
     if not match(expected, actual): 
       raise check50.Mismatch(expected, actual, help=help)
-  
+'''  
