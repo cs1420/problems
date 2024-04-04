c8c3d82cd9d51f9308a21584ba45cad7566a3bc0
GRudolph <george.rudolph@uvu.edu>
2024-04-03 20:26:26 -0600

    Update __init__.py
    uses cs50 checks

--- magical/__init__.py
+++ magical/__init__.py
@@ -19,7 +19,7 @@ def test_pop_0():
 @check50.check(exists)
 def test_nominal_1():
     """handles nominal input values correctly"""
-    check50.run("python3 simulation.py 0 2.5 20 output.txt").exit(0)
+    check50.run("python3 simulation.py 0.1 2.5 20 output.txt").exit(0)
     check50.exists("output.txt")
     actual = open("output.txt").read()
     expected = open("1.txt").read()
@@ -27,46 +27,41 @@ def test_nominal_1():
         raise check50.Mismatch(expected, actual, help="Your output file does not match expected output.")
 
 @check50.check(exists)
-def test2():
-    """handles a height of 2 correctly"""
-    out = check50.run("python3 mario.py").stdin("2").stdout()
-    check_pyramid(out, open("2.txt").read())
+def test_nominal_2():
+    """handles other nominal values correctly"""
+    check50.run("python3 simulation.py 0.1 1.2  80 output.txt").exit(0)
+    check50.exists("output.txt")
+    actual = open("output.txt").read()
+    expected = open("2.txt").read()
+    if not actual == expected:
+        raise check50.Mismatch(expected, actual, help="Your output file does not match expected output.")
+        
+@check50.check(exists)
+def test_non_numeric_rate():
+    """reject non-numeric rate"""
+    check50.run("python3 simulation.py 0.1 pippy 20 output.txt").reject()
 
 @check50.check(exists)
-def test23():
-    """handles a height of 8 correctly"""
-    out = check50.run("python3 mario.py").stdin("8").stdout()
-    check_pyramid(out, open("8.txt").read())
+def test_reject_negative_rate():
+    """rejects a negative rate"""
+    check50.run("python3 simulation.py 0.1 -1 20 output.txt").reject()
 
 @check50.check(exists)
-def test24():
-    """rejects a height of 9, and then accepts a height of 2"""
-    (check50.run("python3 mario.py").stdin("9").reject()
-            .stdin("2").stdout(open("2.txt")).exit(0))
-
-@check50.check(exists)
-def test_reject_foo():
-    """rejects a non-numeric height of "foo" """
-    check50.run("python3 mario.py").stdin("foo").reject()
-
+def test_reject_large_rate():
+    """rejects a rate > 4"""
+    check50.run("python3 simulation.py 0.1 5 20 output.txt").reject()
+    
 @check50.check(exists)
 def test_reject_empty():
-    """rejects a non-numeric height of "" """
-    check50.run("python3 mario.py").stdin("").reject()
+    """rejects when no parameters given"""
+    check50.run("python3 simulation.py").reject()
 
+@check50.check(exists)
+def test_non_numeric_iterations():
+    """reject non-numeric iteration value"""
+    check50.run("python3 simulation.py 0.1 2.5 pippy output.txt").reject()
 
-def check_pyramid(output, correct):
-    if output == correct:
-        return
-
-    output = [line for line in output.splitlines() if line != ""]
-    correct = correct.splitlines()
-
-    help = None
-    if len(output) == len(correct):
-        if all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
-            help = "Did you add too much trailing whitespace to the end of your pyramid?"
-        elif all(ol[1:] == cl for ol, cl in zip(output, correct)):
-            help = "Are you printing an additional character at the beginning of each line?"
-
-    raise check50.Mismatch(correct, output, help=help)
+@check50.check(exists)
+def test_reject_negative_iterations():
+    """rejects a negative rate"""
+    check50.run("python3 simulation.py 0.1 2.5 -1 output.txt").reject()
