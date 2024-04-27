df3527a5a9343a4544620e43a34d661d4a3fefb4
GRudolph <george.rudolph@uvu.edu>
2024-04-27 11:59:53 -0600

    Revert "Update __init__.py"
    This reverts commit a024dcc70a49c5d22f81d0883cb8f0724e6d72f1.

--- magical/__init__.py
+++ magical/__init__.py
@@ -2,40 +2,64 @@ import check50
 
 @check50.check()
 def exists():
-    """library.py exists."""
-    check50.exists("library.py")
-    check50.include("ALG.txt","FMP.txt","PNP.txt","TTL.txt","TSL.txt","WOO.txt")
-    check50.include("ref_ALG.txt","ref_FMP.txt","ref_PNP.txt","ref_TTL.txt","ref_TSL.txt","ref_WOO.txt")
-    
+    """simulation.py exists."""
+    check50.exists("simulation.py")
+    check50.include("1.txt","2.txt")
+
+@check50.check(exists)
+def test_reject_negative():
+    """rejects a negative population percent"""
+    status = check50.run("python3 simulation.py -0.5 2.5 20 output.txt").exit()
+    if status == 0:
+        raise check50.Failure(f"expected to reject negative population")
+
+@check50.check(exists)
+def test_pop_0():
+    """rejects a population percent of 0"""
+    status = check50.run("python3 simulation.py 0 2.5 20 output.txt").exit()
+    if status == 0:
+        raise check50.Failure(f"expected to reject 0 population")
+
 @check50.check(exists)
 def test_nominal_1():
-    """handles ALG file correctly"""
-    check50.run("python3 library.py ALG.txt").exit(0)
-    check50.exists("ALG_book.txt")
-    actual = open("ALG_book.txt").read()
-    expected = open("ref_ALG_book.txt").read()
+    """handles nominal input values correctly"""
+    check50.run("python3 simulation.py 0.1 2.5 20 output.txt").exit(0)
+    check50.exists("output.txt")
+    actual = open("output.txt").read()
+    expected = open("1.txt").read()
     if not actual == expected:
         raise check50.Mismatch(expected, actual, help="Your output file does not match expected output.")
 
 @check50.check(exists)
 def test_nominal_2():
-    """handles TTL file correctly"""
-    check50.run("python3 library.py TTL.txt").exit(0)
-    check50.exists("TTL_book.txt")
-    actual = open("TTL_book.txt").read()
-    expected = open("ref_TTL_book.txt").read()
+    """handles other nominal values correctly"""
+    check50.run("python3 simulation.py 0.1 1.2  80 output.txt").exit(0)
+    check50.exists("output.txt")
+    actual = open("output.txt").read()
+    expected = open("2.txt").read()
     if not actual == expected:
         raise check50.Mismatch(expected, actual, help="Your output file does not match expected output.")
+        
+@check50.check(exists)
+def test_non_numeric_rate():
+    """reject non-numeric rate"""
+    status = check50.run("python3 simulation.py 0 pippy 20 output.txt").exit()
+    if status == 0:
+        raise check50.Failure(f"expected to reject non-numeric rate")
 
 @check50.check(exists)
-def test_nominal_3():
-    """handles WOO file correctly"""
-    check50.run("python3 library.py WOO.txt").exit(0)
-    check50.exists("WOO_book.txt")
-    actual = open("WOO_book.txt").read()
-    expected = open("ref_WOO_book.txt").read()
-    if not actual == expected:
-        raise check50.Mismatch(expected, actual, help="Your output file does not match expected output.")
+def test_reject_negative_rate():
+    """rejects a negative rate"""
+    status = check50.run("python3 simulation.py 0 -1 20 output.txt").exit()
+    if status == 0:
+        raise check50.Failure(f"expected to reject non-numeric rate")
+
+@check50.check(exists)
+def test_reject_large_rate():
+    """rejects a rate > 4"""
+    status = check50.run("python3 simulation.py 0 5 20 output.txt").exit()
+    if status == 0:
+        raise check50.Failure(f"expected to reject non-numeric rate")
     
 @check50.check(exists)
 def test_reject_empty():
