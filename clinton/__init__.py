f2501e22e439ec1fd8add2c5ab0b0164890c1211
GRudolph <george.rudolph@uvu.edu>
2024-04-28 18:15:41 -0600

    fixed existence test text

--- clinton/__init__.py
+++ clinton/__init__.py
@@ -2,7 +2,7 @@ import check50
 
 @check50.check()
 def exists():
-    """random_walk.py exists."""
+    """fact_check.py and other files exist."""
     check50.exists("fact_check.py")
     check50.exists("BLS_private.csv")
     check50.exists("presidents.txt")
